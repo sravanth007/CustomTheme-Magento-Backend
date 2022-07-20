b8d991d3150abd738a12f2c494b1d69ba212ef75
sravanth007 <90679731+sravanth007@users.noreply.github.com>
2022-07-20 19:23:15 +0530

    Add files via upload

---
+++ i95dev/admin/registration.php
@@ -0,0 +1,13 @@
+<?php
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+
+    'i95dev_admin',
+
+    __DIR__
+
+);
+
+?>
